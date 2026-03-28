0e845cb4f49cd18cabf40352898f2e0c9f978f16
Andrei Drazdou <andrei.drazdou@gmail.com>
2026-03-28 23:13:40 +0500

    fix bugs

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import os
 import base64
 import requests
 import json
+import io
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from fastapi import BackgroundTasks
-from responses import BOT_RESPONSES
+from dictionary import BOT_RESPONSES
 
 app = FastAPI()
 
 BOT_TOKEN = os.getenv("TOKEN")
 URL_1C = os.getenv("URL_1C")
+
+class CommandFrom1C(BaseModel):
+    chat_id: int
+    command_code: str
+    extra_text: str = ""
+    file_base64: str = ""
+    file_name: str = ""
 
 def download_file_as_base64(file_id):
 
@@ -74,3 +82,54 @@ async def handle_webhook(request: Request):
         return {"status": "sent_to_1c", "1c_response": response.status_code}
     except Exception as e:
         return {"status": "error", "reason": str(e)}
+
+@app.post("/send_to_bot")
+async def send_to_bot(cmd: CommandFrom1C):
+
+    response_config = BOT_RESPONSES.get(cmd.command_code)
+
+    if not response_config:
+        return {"status": "error", "message": "Unknown command code"}
+
+    text = response_config["text"]
+    if cmd.extra_text:
+        text += f"\n\n{cmd.extra_text}"
+
+    reply_markup = None
+    if response_config.get("buttons"):
+        reply_markup = json.dumps({
+            "keyboard": response_config["buttons"],
+            "resize_keyboard": True
+        })
+
+    if cmd.file_base64:
+
+        file_bytes = base64.b64decode(cmd.file_base64)
+
+        file_io = io.BytesIO(file_bytes)
+        file_io.name = cmd.file_name or "file.jpg"
+
+        is_photo = file_io.name.lower().endswith(('.jpg', '.jpeg', '.png'))
+        method = "sendPhoto" if is_photo else "sendDocument"
+        file_type = "photo" if is_photo else "document"
+
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/{method}"
+        
+        files = {file_type: (file_io.name, file_io)}
+        data = {
+            "chat_id": cmd.chat_id,
+            "caption": text, # В методах с файлами текст сообщения идет в поле caption
+            "reply_markup": reply_markup
+        }
+        
+        tg_res = requests.post(url, data=data, files=files)
+    else:
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": cmd.chat_id,
+            "text": text,
+            "reply_markup": reply_markup
+        }
+        tg_res = requests.post(url, json=payload)
+
+    return {"status": "ok", "tg_code": tg_res.status_code}
