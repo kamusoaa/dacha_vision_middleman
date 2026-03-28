f3a4d7cfdf0ee291d6cdce3e5cdcec3241de9266
Andrei Drazdou <andrei.drazdou@gmail.com>
2026-03-28 23:19:14 +0500

    Add responses and fix Dockerfile

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,7 @@
+# dictionary.py
 # Словарь с настройками кнопок и текстов
 
-ВОТ_REPSPONSES = {
+BOT_RESPONSES = {
     "FORBIDDEN": {"text": "🚫 Forbidden! Unauthorized user. Contact admin for help."},
     "WELCOME": {
         "text": "🏡 $Param1, система Dacha Vision AI приветствует вас!\nСтатус: На связи!",
